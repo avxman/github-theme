8ae9cd06db674118e5aad87c0fe8e849e69d1772
dvacomDev <support@dvacom.ua>
2022-01-21 10:57:35 +0200

    Release 0.0.2

---
+++ app/Http/Controllers/GithubController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Response;
+
+class GithubController extends Controller
+{
+
+    public function index(){
+        return Response::json(['status'=>true, 'message'=>['It\'s me!!!']]);
+    }
+
+}
