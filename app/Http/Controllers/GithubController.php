3a86dc30e16d645775ff41a9033382b70b425bda
dvacomDev <support@dvacom.ua>
2022-01-21 11:31:11 +0200

    Release 0.0.5

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Avxman\Github\Facades\GithubFacade;
 use Illuminate\Support\Facades\Response;
 
 class GithubController extends Controller
 {
 
     public function index(){
+        GithubFacade::instance();
         return Response::json(['status'=>true, 'message'=>['It\'s!!!']]);
     }
 
